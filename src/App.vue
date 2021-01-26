ca6ae63e16afb80835ba77fb43c79b22ca45a81a
Chris <chrasse@hotmail.com>
2021-01-26 14:13:42 +0100

    Background pic and Navbar content and styling

--- src/App.vue
+++ src/App.vue
@@ -17,4 +17,28 @@ export default {
 
 <style>
 
+  @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@400;600&display=swap');
+
+  body {
+    background: url('./assets/img/forest-landscape.jpg');
+    margin: 0;
+    background-size: cover;
+    background-position: center;
+    min-height: 100vh;
+
+    font-family: 'Quicksand', sans-serif;
+    color: #F7F9FB;
+
+
+    /*
+     Color scheme
+     #687864  dark green
+     #31708E  darker blue-ish
+     #5085A5  more grey blue-ish
+     #8FC1E3  lighter blue
+     #F7F9FB  soft light/white-ish
+
+     */
+  }
+
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -12,6 +12,36 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+#navbar {
+    background-color: #687864;
+    text-align: center;
+    padding: 2rem;
+}
+
+a {
+    text-decoration: none;
+    color: #F7F9FB;
+    padding: 0.5rem 0.8rem;
+    margin: 0 1.5rem;
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    font-weight: 600;
+    transition: 0.3s;
+}
+a:hover{
+    color: #8fc1e3;
+}
+
+/*
+     Color scheme
+     #687864  dark green
+     #31708E  darker blue-ish
+     #5085A5  more grey blue-ish
+     #8FC1E3  lighter blue
+     #F7F9FB  soft light/white-ish
+
+     */
 
 </style>
